1b66ddf33b8d4cea9578417c7ba510d00e34eccf
Zitouni Abdeldjallil <43116001+Zabdeldjallil@users.noreply.github.com>
2022-12-23 15:04:50 +0100

    Update pharmacy.js

--- pharmacy.js
+++ pharmacy.js
@@ -10,70 +10,66 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-    if(this.drugs[i].benefit<=50){
-        if (
-          this.drugs[i].name != "Herbal Tea" &&
-            this.drugs[i].name != "Fervex"&&
-            this.drugs[i].name!=="Dafalgan"
-        ) {
-          if (this.drugs[i].benefit > 0) {
-            if (this.drugs[i].name != "Magic Pill"&& this.drugs[i].name!=="Dafalgan") {
-              this.drugs[i].benefit = this.drugs[i].benefit - 1;
+  updateBenefitValue(){
+    for (let index = 0; index < this.drugs.length; index++) {
+      //benefit is never bigger than 50
+      if(this.drugs[index].benefit>50){
+        throw new Error("can't be more than 50")
+      }
+      //magic pill never expires nor decrease in benefit
+
+    if(this.drugs[index].name!=="Magic Pill"){
+        //benefit is never negative
+        //once the expiaration date passed it decreases twice as fast
+        if(this.drugs[index].expiresIn<=0){
+        if(this.drugs[index].name!=="Fervex"){
+            //if it's herbal tea it should increase in benefits
+            if(this.drugs[index].name!=="Herbal Tea"){
+              this.drugs[index].benefit=this.drugs[index].benefit-2 >= 0 ? this.drugs[index].benefit-2 :0
+            }else{
+              this.drugs[index].benefit=this.drugs[index].benefit + 2 < 50 ? this.drugs[index].benefit+2 :50
+            }
+          }else{
+            if(this.drugs[index].name==="Dafalgan"){
+              this.drugs[index].benefit=this.drugs[index].benefit-4 >0 ? this.drugs[index].benefit-4:0
+            }else{
+            //put benefit to 0 when it's fervex
+                  this.drugs[index].benefit=0
             }
           }
-        } else {
-          if (this.drugs[i].benefit < 50&&this.drugs[i].name!=="Dafalgan") {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-            if (this.drugs[i].name == "Fervex") {
-              if (this.drugs[i].expiresIn < 11) {
-                if (this.drugs[i].benefit < 50) {
-                  this.drugs[i].benefit = this.drugs[i].benefit + 1;
+        }else{
+          if(this.drugs[index].name!=="Herbal Tea"&&this.drugs[index].name!=="Fervex"&&this.drugs[index].name!=="Dafalgan"){
+            this.drugs[index].benefit=this.drugs[index].benefit-1 >= 0 ?this.drugs[index].benefit-1:0
+          }else{
+
+            //if Fervex expiresIn more than 10 days it's benefit keeps increasing
+            if(this.drugs[index].name==="Fervex"&&this.drugs[index].expiresIn>10){
+            this.drugs[index].benefit=this.drugs[index].benefit + 1 < 50 ? this.drugs[index].benefit+1 :50
+            }else{
+              //if it's Fervex and it's expiring in 10 days or less
+              if(this.drugs[index].name==="Fervex"&& this.drugs[index].expiresIn<=10&& this.drugs[index].expiresIn>=5){
+            this.drugs[index].benefit=this.drugs[index].benefit + 2 < 50 ? this.drugs[index].benefit+2 :50
+              }else{
+
+            //if it's Dafalgan then it decreases twice as fast as normal drugs
+            if(this.drugs[index].name==="Dafalgan"){
+
+            this.drugs[index].benefit=this.drugs[index].benefit - 2 >= 0 ? this.drugs[index].benefit-2 :0
+            }else{
+              //if it's not Fervex nor Dafalgan then everything happens normally
+            this.drugs[index].benefit=this.drugs[index].benefit + 1 < 50 ? this.drugs[index].benefit+1 :50
                 }
-              }
-              if (this.drugs[i].expiresIn < 6) {
-                if (this.drugs[i].benefit < 50) {
-                  this.drugs[i].benefit = this.drugs[i].benefit + 1;
-                }
-              }
-            }
-          }
-        }
-        if (this.drugs[i].name === "Dafalgan"&&this.drugs[i].expiresIn>0) {
-              this.drugs[i].benefit = this.drugs[i].benefit - 2 >=0 ? this.drugs[i].benefit -2:this.drugs[i].benefit 
-            }
-        if(this.drugs[i].name === "Dafalgan"&&this.drugs[i].expiresIn<=0){
-              this.drugs[i].benefit = this.drugs[i].benefit - 4>=0?this.drugs[i].benefit-4:this.drugs[i].benefit
-            }
-        if (this.drugs[i].name != "Magic Pill") {
-          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-        }
-        if (this.drugs[i].expiresIn < 0) {
-        if(this.drugs[i].name!=="Dafalgan"){
-            if (this.drugs[i].name != "Herbal Tea") {
-              if (this.drugs[i].name != "Fervex") {
-                if (this.drugs[i].benefit > 0) {
-                  if (this.drugs[i].name != "Magic Pill") {
-                    this.drugs[i].benefit = this.drugs[i].benefit - 1;
-                  }
-                }
-              } else {
-                this.drugs[i].benefit =
-                  this.drugs[i].benefit - this.drugs[i].benefit;
-              }
-            } else {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                
               }
             }
           }
         }
-      }else {
-        throw  new Error("can't be more than 50")
+        this.drugs[index].expiresIn=this.drugs[index].expiresIn-1
+      }else{
+        //decrease the expiredIn value of Magic Pill
+        this.drugs[index].expiresIn-=1
       }
     }
-
-    return this.drugs;
+    return this.drugs
   }
 }
